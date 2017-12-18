a9cd9d7165c2794758fe4cc725866bb3f33bcbed
Udvari Tibor <tudvari@tudvari.com>
2017-12-18 22:01:02 +0100

    test refactor with ini

--- tests/GeneratorTests.js
+++ tests/GeneratorTests.js
@@ -3,6 +3,7 @@
 'use strict'
 
 var should = require('should')
+var ini = require('ini')
 var Generator = require('../index')
 
 
@@ -11,26 +12,29 @@ describe('Generator Tests', function () {
 		Generator.generateShareConfig('testShareName', {}, function (err, result) {
 			should.not.exist(err)
 			result.should.startWith('[testShareName]')
+			result.should.endWith('[testShareName]\n')
 			done()
 		})
 	})
 
 	it('Params has 1 item', function (done) {
-		Generator.generateShareConfig('testShareName', {'testkey': 'testValue'}, function (err, result) {
+		var testData1 = {'testkey1': 'testValue1', 'testkey2': 'testValue2'}
+		Generator.generateShareConfig('testShareName', testData1, function (err, result) {
 			should.not.exist(err)
-			result.should.startWith('[testShareName]')
-			result.should.endWith('testkey = testValue\n')
+			var generatedIni = ini.encode(testData1, {section: 'testShareName', whitespace: true})
+			result.should.be.eql(generatedIni)
 			done()
 		})
 	})
 
-	it('Params has more item', function (done) {
-		Generator.generateShareConfig('testShareName', {'testkey1': 'testValue1', 'testkey2': 'testValue2'}, function (err, result) {
+	it('Params has more item with ini', function (done) {
+		var testData1 = {'testkey1': 'testValue1', 'testkey2': 'testValue2'}
+		Generator.generateShareConfig('testShareName', testData1, function (err, result) {
 			should.not.exist(err)
-			result.should.startWith('[testShareName]')
-			result.should.endWith('testkey2 = testValue2\n')
-			result.should.containEql('testkey1 = testValue1\n')
+			var generatedIni = ini.encode(testData1, {section: 'testShareName', whitespace: true})
+			result.should.be.eql(generatedIni)
 			done()
 		})
 	})
+
 })
