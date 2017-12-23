d252cea4f121e76d15410271d6e9b09b119df858
Udvari Tibor <tudvari@tudvari.com>
2017-12-23 17:01:02 +0100

    updateConfig

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+var fs = require('fs')
+var async = require('async')
+var ini = require('ini')
+
 var Generator = {
 	generateShareConfig: function (shareName, params, callback) {
 		var generatedConfig = {}
@@ -14,12 +18,44 @@ var Generator = {
 
 	generateSection: function (shareName, fileName, existingConfig, callback) {
 		if (!shareName) return callback(new Error('shareName can\'t be emptry'))
+
 		existingConfig[shareName] = {}
 		existingConfig[shareName]['include'] = fileName
+
 		return callback(null, existingConfig)
 	},
 
-	writeConfig: function (config, sectionFileName, callback) {
+	updateConfig: function (configPath, sectionFileName, sectionName, sectionParams, configReload, callback) {
+		async.series([
+			function (cb) {
+				// generate Share Config
+				this.generateShareConfig(sectionName, sectionParams, function (err, generatedShareConfig) {
+					if (err) cb(err)
+
+					fs.writeFileSync(sectionFileName, ini.stringify(generatedShareConfig))
+					cb(null, true)
+				})
+			},
+			function (cb) {
+				// Include Share Config
+				var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
+
+				this.generateSection(sectionName, sectionFileName, config, function (err, generatedConfig) {
+					if (err) cb(err)
+
+					fs.writeFileSync(sectionFileName, ini.stringify( generatedConfig))
+					cb(null, true)
+				})
+			},
+			function (cb) {
+				// Validate Config
+				cb(null, true)
+			}], function (err, results) {
+			if (err) throw err
+			if (configReload) {
+				// Reload Config
+			}
+		})
 	}
 }
 
